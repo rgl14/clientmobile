3473cf5efbbb6963e5ae8b418f9f0824492cd129
rgl14 <52996923+rgl14@users.noreply.github.com>
2019-11-14 22:09:20 +0530

    made fullmarket scorecard and matchodds design

--- src/app/inplay/inplay.component.ts
+++ src/app/inplay/inplay.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-inplay',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./inplay.component.scss']
 })
 export class InplayComponent implements OnInit {
-
+  @Output() someEvent = new EventEmitter<string>();
   constructor() { }
 
   ngOnInit() {
